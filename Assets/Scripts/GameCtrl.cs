50a50f364fab958e9332ef3ae9fb1f0b01d954b9
KotaGit <Kota7arai@gmail.com>
2021-08-19 23:04:22 +0900

    駒を交互に置く
    駒を交互に置く

--- Assets/Scripts/GameCtrl.cs
+++ Assets/Scripts/GameCtrl.cs
@@ -7,13 +7,27 @@ public class GameCtrl : MonoBehaviour
     //プライぺート変数
     //10*10のint型2次元配列
     private int[,] squares = new int[10, 10];
+
     //EMPTY=0、WHITE=0、BLACK=0;
     private const int EMPTY = 0;
     private const int WHITE = 1;
     private const int BLACK = -1;
 
+    //現在のプレイヤー（初期プレイヤーは白）
+    private int currentPlayer = WHITE;
+
+    //カメラ情報
+    private Camera camera_object;
+    private RaycastHit hit;
+
+    //プレファブ
+    public GameObject whiteStone;
+    public GameObject blackStone;
+
     void Start()
     {
+        //カメラ情報を取得
+        camera_object = GameObject.Find("Main Camera").GetComponent<Camera>();
         //配列を初期化
         InitializeArray();
 
@@ -24,7 +38,51 @@ public class GameCtrl : MonoBehaviour
     
     void Update()
     {
-        
+        //マウスがクリックされたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスのポジションを取得してRayに代入
+            Ray ray = camera_object.ScreenPointToRay(Input.mousePosition);
+
+            //マウスのポジションからRayを投げて何かにあたったらhitにいれる　
+            if(Physics.Raycast(ray,out hit))
+            {
+                //x、zの値を取得
+                int x = (int)hit.collider.gameObject.transform.position.x;
+                int z = (int)hit.collider.gameObject.transform.position.z;
+
+                //マスが空のとき
+                if(squares[z,x] == EMPTY)
+                {
+                    //白のターンのとき
+                    if(currentPlayer == WHITE)
+                    {
+                        //Squaresの値を更新
+                        squares[z, x] = WHITE;
+
+                        //Stoneを出力
+                        GameObject stone = Instantiate(whiteStone);
+                        stone.transform.position = hit.collider.gameObject.transform.position;
+
+                        //Playerを交代
+                        currentPlayer = BLACK;
+                    }
+                    //黒のターンのとき
+                    else if(currentPlayer == BLACK)
+                    {
+                        //Squaresの値を更新
+                        squares[z, x] = BLACK;
+
+                        //Stoneを出力
+                        GameObject stone = Instantiate(blackStone);
+                        stone.transform.position = hit.collider.gameObject.transform.position;
+
+                        //Playerを交代
+                        currentPlayer = WHITE;
+                    }
+                }
+            }
+        }
     }
 
     private void InitializeArray()
